cce807f5077eda02492f3bc12e06efd3c87dad2e
Traceless <traceless0929@outlook.com>
2019-05-05 10:43:32 +0800

    更新SDK

--- Native.Csharp/App/Core/LibExport.cs
+++ Native.Csharp/App/Core/LibExport.cs
@@ -56,10 +56,10 @@ namespace Native.Csharp.App.Core
 		{
 			// 请勿随意修改
 			//
-			// 当前项目名称: Native.Csharp
+			// 当前项目名称: site.traceless.SmartTv2
 			// Api版本: 9
 
-			return string.Format ("{0},{1}", 9, "Native.Csharp");
+			return string.Format ("{0},{1}", 9, "site.traceless.SmartTv2");
 		}
 
 		/// <summary>
