117ed1530cbd9ab735872281c5b63a0cf8625906
Traceless <Traceless0929@outlook.com>
2020-02-11 16:20:03 +0800

    1

--- Site.Traceless.SamrtT.Code/Func/JxTask.cs
+++ Site.Traceless.SamrtT.Code/Func/JxTask.cs
@@ -13,43 +13,51 @@ namespace Site.Traceless.SamrtT.Code.Func
         public static string getTask()
         {
             StringBuilder sb = new StringBuilder();
-            JObject resp = Tools.Http.HttpHelper.GetAPI<JObject>(@"http://nico.nicemoe.cn/moe/content/");
-            if (resp["code"].ToString() != "200")
+            try
             {
-                sb.AppendLine("[查日常]查询失败，请联系管理员QAQ");
-            }
-            else
-            {
-                JToken timeInfo = resp["时间"].FirstOrDefault();
-                JToken dayInfo = resp["日常"].FirstOrDefault();
-                JToken weekInfo = resp["周常"].FirstOrDefault();
-                if (timeInfo.HasValues)
+                JObject resp = Tools.Http.HttpHelper.GetAPI<JObject>(@"http://nico.nicemoe.cn/moe/content/");
+                if (resp["code"].ToString() != "200")
                 {
-                    sb.AppendLine("[查日常] "+timeInfo["日期"].ToString() + " " + timeInfo["星期"].ToString());
+                    sb.AppendLine("[查日常]查询失败，请联系管理员QAQ");
                 }
-                if (dayInfo.HasValues)
+                else
                 {
-                    sb.AppendLine(Environment.NewLine + "---------日常---------");
-                    foreach (var dayItem in dayInfo)
+                    JToken timeInfo = resp["时间"].FirstOrDefault();
+                    JToken dayInfo = resp["日常"].FirstOrDefault();
+                    JToken weekInfo = resp["周常"].FirstOrDefault();
+                    if (timeInfo.HasValues)
                     {
-                        sb.AppendLine("「" + dayItem.Path.Split('.').LastOrDefault() + "」" + dayItem.FirstOrDefault().ToString() + "");
+                        sb.AppendLine("[查日常] " + timeInfo["日期"].ToString() + " " + timeInfo["星期"].ToString());
                     }
-                    sb.AppendLine("「备注」日常七点刷新");
-                }
-                if (weekInfo.HasValues)
-                {
-                    sb.AppendLine(Environment.NewLine + "---------周常---------");
-                    foreach (var weekItem in weekInfo)
+                    if (dayInfo.HasValues)
                     {
-                        sb.AppendLine("「" + weekItem.Path.Split('.').LastOrDefault().Replace("武林通鉴·", "") + "」"
-                                      + Environment.NewLine+"  " + weekItem.FirstOrDefault().ToString().Replace(",", Environment.NewLine+ "  ") + "");
+                        sb.AppendLine(Environment.NewLine + "---------日常---------");
+                        foreach (var dayItem in dayInfo)
+                        {
+                            sb.AppendLine("「" + dayItem.Path.Split('.').LastOrDefault() + "」" + dayItem.FirstOrDefault().ToString() + "");
+                        }
+                        sb.AppendLine("「备注」日常七点刷新");
                     }
-                    sb.AppendLine("「备注」周常十二点刷新");
-                }
+                    if (weekInfo.HasValues)
+                    {
+                        sb.AppendLine(Environment.NewLine + "---------周常---------");
+                        foreach (var weekItem in weekInfo)
+                        {
+                            sb.AppendLine("「" + weekItem.Path.Split('.').LastOrDefault().Replace("武林通鉴·", "") + "」"
+                                          + Environment.NewLine + "  " + weekItem.FirstOrDefault().ToString().Replace(",", Environment.NewLine + "  ") + "");
+                        }
+                        sb.AppendLine("「备注」周常十二点刷新");
+                    }
 
-                sb.AppendLine(Environment.NewLine + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                    sb.AppendLine(Environment.NewLine + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                }
+                return sb.ToString();
             }
-            return sb.ToString();
+            catch (Exception e)
+            {
+                sb.AppendLine("[查日常]QAQ日常服务器异常，已通知管理员");
+                return sb.ToString();
+            }
         }
     }
 }
