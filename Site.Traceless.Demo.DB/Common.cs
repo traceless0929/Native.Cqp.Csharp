6591d1740116d5d3e9bdc58c818ce897f5bd168f
Traceless <traceless0929@outlook.com>
2020-03-27 16:44:01 +0800

    更新到SQLITE整合

---
+++ Site.Traceless.Demo.DB/Common.cs
@@ -0,0 +1,12 @@
+﻿using Native.Sdk.Cqp;
+
+namespace Site.Traceless.Demo.DB
+{
+    public static class Common
+    {
+        public static CQApi CqApi { get; set; }
+        public static CQLog CqLog { get; set; }
+        public static string DbPath { get; set; }
+        public static SQLiteHelper sqliteHelper { get; set; }
+    }
+}
---
+++ Site.Traceless.Demo.DB/SQLiteHelper.cs
@@ -0,0 +1,41 @@
+﻿using SQLite;
+using System.Collections.Generic;
+
+namespace Site.Traceless.Demo.DB
+{
+    public class SQLiteHelper
+    {
+        public SQLiteConnection db;
+
+        public SQLiteHelper(string connstr)
+        {
+            db = new SQLiteConnection(connstr);
+            //db.CreateTable<T_Account_Bind>();//表已存在不会重复创建
+        }
+
+        public int Add<T>(T model)
+        {
+            return db.Insert(model);
+        }
+
+        public int Update<T>(T model)
+        {
+            return db.Update(model);
+        }
+
+        public int Delete<T>(T model)
+        {
+            return db.Update(model);
+        }
+
+        public List<T> Query<T>(string sql) where T : new()
+        {
+            return db.Query<T>(sql);
+        }
+
+        public int Execute(string sql)
+        {
+            return db.Execute(sql);
+        }
+    }
+}
